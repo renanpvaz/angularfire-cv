d0018ed40e5f64934a7e1b6f40c44db85e3ef22e
Renan Vaz <renanp.vaz@outlook.com>
2016-05-17 21:43:54 -0300

    fix responsiveness

--- app/scripts/sidebar/sidebar.directive.js
+++ app/scripts/sidebar/sidebar.directive.js
@@ -5,23 +5,25 @@
       .module('angularfireCvApp')
       .directive('cvSidebar', sideBar);
 
-      sideBar.$inject = ['cvService', '$rootScope'];
+      sideBar.$inject = ['cvService', '$rootScope', '$location'];
 
-      function sideBar(cvService, $rootScope) {
+      function sideBar(cvService, $rootScope, $location) {
         return {
           templateUrl: 'template/sidebar.directive.html',
           restrict: 'E',
           scope: {},
           controller: function() {
             var vm = this;
+            var name = $location.path().split('/')[2];
 
             vm.open = false;
-            vm.sections = cvService.sections('renan')
+            vm.sections = cvService.sections(name)
               .sort(function(left, right){
                 return left.position > right.position;
               });
 
             vm.togglePosition = function(section) {
+              vm.active = section.$id;
               $rootScope.$broadcast('activeSectionToggle', { id: section.$id });
             };
 
